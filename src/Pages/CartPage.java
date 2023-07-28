d6ac245620609cb01827b7502efbda14aaabd364
karanAtreya1986 <karan1988@gmail.com>
2023-07-28 12:49:05 +0200

    updated cart page

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -6,6 +6,8 @@ public class CartPage {
 		// TODO Auto-generated method stub
 		
 		System.out.println("cart page added newly");
+		
+		System.out.println("cart page added newly added another feature");
 
 	}
 
