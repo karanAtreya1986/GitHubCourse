2a8c1e02ba6493175e72985e512acce2771c19c8
karanAtreya1986 <karan1988@gmail.com>
2023-08-03 16:00:35 +0200

    feature 2 is added

--- src/Pages/Feature1.java
+++ src/Pages/Feature1.java
@@ -4,6 +4,8 @@ public class Feature1 {
 	
 	public static void main(String[] args) {
 		System.out.println("feature one is developed");
+		
+		System.out.println("feature two is developed");
 	}
 
 }
