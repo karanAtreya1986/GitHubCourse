1ce0f6b51820b8be40343493a063f0e8ef191669
karanAtreya1986 <karan1988@gmail.com>
2023-08-08 17:15:10 +0200

    added cart feature page

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -3,8 +3,12 @@ package Pages;
 public class CartPage {
 	
 	public void cart() {
-		String namedev2="cart dev 2 has written";
-		System.out.println(namedev2);
+		String namedev1="cart done by dev 1";
+		System.out.println(namedev1);
+		if(true) {
+			System.out.println("cart is ready ..........");
+		}
+
 	}
 
 }
