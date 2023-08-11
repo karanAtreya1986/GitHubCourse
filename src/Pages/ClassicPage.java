cdf2a45133ffed8b005add04f534466090043723
karanAtreya1986 <karan1988@gmail.com>
2023-08-11 08:45:19 +0200

    trying some scenarios

--- src/Pages/ClassicPage.java
+++ src/Pages/ClassicPage.java
@@ -8,6 +8,7 @@ public class ClassicPage {
 		int k1=200;
 		int div=k1/k;
 		System.out.println(div);
+		System.out.println("hi");
     System.out.println("bye code from remote");
     System.out.println("Accepted our friends code");
 
